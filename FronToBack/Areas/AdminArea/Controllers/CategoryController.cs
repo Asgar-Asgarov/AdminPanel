ca5f0add61e07e3a6ac9d166605734ba09f2e7ef
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-03-01 12:36:20 +0400

    refactor:category view was changed

--- FronToBack/Areas/AdminArea/Controllers/CategoryController.cs
+++ FronToBack/Areas/AdminArea/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
-
+using FronToBack.DAL;
 namespace FronToBack.Areas.AdminArea.Controllers;
 
 [Area("AdminArea")]
 public class CategoryController:Controller
 {
+    private readonly AppDbContext _appDbCOntext;
+
+    public CategoryController(AppDbContext appDbCOntext)
+    {
+        _appDbCOntext = appDbCOntext;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        return View(_appDbCOntext.Categories.ToList());
     }
 }
--- FronToBack/Models/Category.cs
+++ FronToBack/Models/Category.cs
@@ -3,5 +3,6 @@ namespace FronToBack.Models;
  {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
     public List<Product> products {get;set;}
  }
